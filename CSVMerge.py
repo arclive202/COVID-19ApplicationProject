ad7699b5fb35c4919ae11a2c133bd954b952b398
arclive202 <48155936+arclive202@users.noreply.github.com>
2020-03-29 10:43:16 +0530

    Add files via upload

---
+++ CSVMerge.py
@@ -0,0 +1,17 @@
+﻿import os
+import pandas as pd
+
+filename = "C:\\Users\\aditya.roychoudhary\\Desktop\\Country\\Countries.csv"
+    
+
+
+for root, dirs, files in os.walk("C:\\Users\\aditya.roychoudhary\\Desktop\\Country\\"):
+    for file in files:
+        if file.endswith(".csv"):
+            tempdf = pd.read_csv(os.path.join(root, file))
+            if not os.path.isfile(filename):
+                tempdf.to_csv(filename, header='column_names')
+            else:
+                tempdf.to_csv(filename, mode='a', header=False)
+            del tempdf
+            
